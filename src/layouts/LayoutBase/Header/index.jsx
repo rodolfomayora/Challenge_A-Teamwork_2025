980d9cdfc2ed9fea8b1d0b2e7ac30d822b15713e
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-13 14:53:28 -0400

    feat(layouts): add initial mobile menu

--- src/layouts/LayoutBase/Header/index.jsx
+++ src/layouts/LayoutBase/Header/index.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 export function Header() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const handleToggle = () => {
+    setIsExpanded((isExpanded) => !isExpanded);
+  }
+  const buttonIcon = isExpanded ?
+    <img className={styles.toggleButton} src="/icons/menu-open.png" width="34" height="34" /> :
+    <img className={styles.toggleButton} src="/icons/menu-close.png" width="34" height="34" /> ; 
+
   return (
     <header className={styles.Header}>
       {/* menu desktop */}
@@ -11,23 +20,32 @@ export function Header() {
               <img className={styles.logo} src="/assets/VRUME-white-with-red-dot.png" alt="Vrume Logo" width="140" height="50" />
             </a>
           </div>
-          <div>
-            <img className={styles.toggleButton} src="/icons/menu-close.png" width="34" height="34" />
-            {/* <img className={styles.toggleButton} src="/icons/menu-open.png" width="34" height="34" /> */}
+          <div onClick={handleToggle}>
+            {buttonIcon}
           </div>
           <div className={styles.desktopNavigationWrapper}>
             <ul className={styles.desktopNavigation}>
-              <li><a className={styles.link}>Home</a></li>
-              <li><a className={styles.link}>Advertisers</a></li>
-              <li><a className={styles.link}>Publisher</a></li>
-              <li><a className={styles.link}>Case Studies</a></li>
-              <li><a className={styles.link}>Contact us</a></li>
+              <li><a className={styles.link} href="#">Home</a></li>
+              <li><a className={styles.link} href="#">Advertisers</a></li>
+              <li><a className={styles.link} href="#">Publisher</a></li>
+              <li><a className={styles.link} href="#">Case Studies</a></li>
+              <li><a className={styles.link} href="#">Contact us</a></li>
             </ul>
           </div>
         </nav>
       </div>
       {/* menu movile */}
-      {/* <nav></nav> */}
+      <nav className={styles.mobileNavigationWrapper} data-expanded={isExpanded}>
+        <div className={styles.container}>
+          <ul className={styles.mobileNavigation}>
+            <li><a className={styles.link} href="#">Home</a></li>
+            <li><a className={styles.link} href="#">Advertisers</a></li>
+            <li><a className={styles.link} href="#">Publisher</a></li>
+            <li><a className={styles.link} href="#">Case Studies</a></li>
+            <li><a className={styles.link} href="#">Contact us</a></li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 }
