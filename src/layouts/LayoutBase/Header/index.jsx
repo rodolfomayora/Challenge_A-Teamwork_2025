8d233cf0651622a98d4cc3ff2817645021bf8b45
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-13 12:40:18 -0400

    feat(layouts): add desktop menu links

--- src/layouts/LayoutBase/Header/index.jsx
+++ src/layouts/LayoutBase/Header/index.jsx
@@ -6,7 +6,7 @@ export function Header() {
       {/* menu desktop */}
       <div className={styles.container}>
         <nav className={styles.navigation}>
-          <div>
+          <div className={styles.logoWrapper}>
             <a href="#">
               <img className={styles.logo} src="/assets/VRUME-white-with-red-dot.png" alt="Vrume Logo" width="140" height="50" />
             </a>
@@ -15,13 +15,15 @@ export function Header() {
             <img className={styles.toggleButton} src="/icons/menu-close.png" width="34" height="34" />
             {/* <img className={styles.toggleButton} src="/icons/menu-open.png" width="34" height="34" /> */}
           </div>
-          {/*<ul className={styles.desktopNavigation}>
-            <li><a>Home</a></li>
-            <li><a>Advertisers</a></li>
-            <li><a>Publisher</a></li>
-            <li><a>Case Studies</a></li>
-            <li><a>Contact us</a></li>
-          </ul> */}
+          <div className={styles.desktopNavigationWrapper}>
+            <ul className={styles.desktopNavigation}>
+              <li><a className={styles.link}>Home</a></li>
+              <li><a className={styles.link}>Advertisers</a></li>
+              <li><a className={styles.link}>Publisher</a></li>
+              <li><a className={styles.link}>Case Studies</a></li>
+              <li><a className={styles.link}>Contact us</a></li>
+            </ul>
+          </div>
         </nav>
       </div>
       {/* menu movile */}
