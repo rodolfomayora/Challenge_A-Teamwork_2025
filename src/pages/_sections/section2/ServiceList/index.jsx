2bec61effed422686944ddd3cf577ece279434a6
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-15 18:08:46 -0400

    feat(pages): add full responsive section-2

--- src/pages/_sections/section2/ServiceList/index.jsx
+++ src/pages/_sections/section2/ServiceList/index.jsx
@@ -1,79 +1,45 @@
 import styles from './styles.module.css';
+import { Card } from './Card';
 
 export function ServiceList() {
   return (
     <ul className={styles.ServiceList}>
       <li>
-        <div className={styles.card}>
-          <img
-            className={styles.image}
-            src="/assets/CPC-CPL-Performance-Marketing.png"
-            alt="Performance"
-            width="120" height="120"
-          />
-          <div className={styles.info}>
-            <span className={styles.title}>CPC & CPL Performance Marketing</span>
-            <p className={styles.text}>Our <strong>performance-based marketing</strong> models give you the <strong>power</strong> to only pay for what works. Whether it’s <strong>CPC</strong> (Cost Per Click) or <strong></strong> (Cost Per Lead), our model ensures that you’re investing in conversions and measurable results.</p>
-          </div>  
-        </div>
+        <Card
+          image="/assets/CPC-CPL-Performance-Marketing.png"
+          title="CPC & CPL Performance Marketing"
+          text={<>Our <strong>performance-based marketing</strong> models give you the <strong>power</strong> to only pay for what works. Whether it’s <strong>CPC</strong> (Cost Per Click) or <strong></strong> (Cost Per Lead), our model ensures that you’re investing in conversions and measurable results.</>}
+        />
       </li>
       <li>
-        <div className={styles.card}>
-          <img
-            className={styles.image}
-            src="/assets/Targeted-Ads.png"
-            alt="Targeted"
-            width="120" height="120"
-          />
-          <div className={styles.info}>
-            <span className={styles.title}>Targeted Ads That Reach Your Ideal Audience</span>
-            <p className={styles.text}>Our advanced targeting ensures that your message reaches the <strong>right people</strong> at the right time.  <strong>native ads</strong> and <strong>targeted social media campaigns,</strong> you’re not just throwing ads into the void — you’re speaking directly to a hand-picked audience primed to take action.</p>
-          </div>
-        </div>
+        <Card
+          image="/assets/Targeted-Ads.png"
+          title="Targeted Ads That Reach Your Ideal Audience"
+          text={<>Our advanced targeting ensures that your message reaches the <strong>right people</strong> at the right time.  <strong>native ads</strong> and <strong>targeted social media campaigns,</strong> you’re not just throwing ads into the void — you’re speaking directly to a hand-picked audience primed to take action.</>}
+        />
       </li>
       <li>
-        <div className={styles.card}>
-          <img
-            className={styles.image}
-            src="/assets/Social-Media-Email-Marketing-Expertise.png"
-            alt="Social"
-            width="120" height="120"
-          />
-          <div className={styles.info}>
-            <span className={styles.title}>Social Media & Email Marketing Expertise</span>
-            <p className={styles.text}>Leverage the power of <strong>email marketing, social networking in marketing,</strong> and <strong>affiliate strategies</strong> to build a list of loyal subscribers, followers, and buyers. We craft email <strong>campaigns</strong> that cut through the noise, driving <strong>conversions</strong> and improving <strong>customer retention.</strong></p>
-          </div>
-        </div>
+        <Card
+          image="/assets/Social-Media-Email-Marketing-Expertise.png"
+          title="Social Media & Email Marketing Expertise"
+          text={<>Leverage the power of <strong>email marketing, social networking in marketing,</strong> and <strong>affiliate strategies</strong> to build a list of loyal subscribers, followers, and buyers. We craft email <strong>campaigns</strong> that cut through the noise, driving <strong>conversions</strong> and improving <strong>customer retention.</strong></>}
+        />
       </li>
       <li>
-        <div className={styles.card}>
-          <img
-            className={styles.image}
-            src="/assets/Affiliate-Networks-Marketing.png"
-            alt="Affiliate"
-            width="120" height="120"
-          />
-          <div className={styles.info}>
-            <span className={styles.title}>Affiliate Networks & Marketing</span>
-            <p className={styles.text}>Scale your business with high-performing <strong>affiliate marketing</strong> strategies. By partnering with our extensive <strong>affiliate network</strong>, you gain access to a world of <strong>qualified leads</strong> and <strong>targeted marketing opportunities</strong> that will supercharge your growth.</p>
-          </div>
-        </div>
+        <Card
+          image="/assets/Affiliate-Networks-Marketing.png"
+          title="Affiliate Networks & Marketing"
+          text={<>Scale your business with high-performing <strong>affiliate marketing</strong> strategies. By partnering with our extensive <strong>affiliate network</strong>, you gain access to a world of <strong>qualified leads</strong> and <strong>targeted marketing opportunities</strong> that will supercharge your growth.</>}
+        />
       </li>
       <li>
-        <div className={styles.card}>
-          <img
-            className={styles.image}
-            src="/assets/Premium-Traffic.png"
-            alt="Premium"
-            width="120" height="120"
-          />
-          <div className={styles.info}>
-            <span className={styles.title}>Premium Traffic Sources</span>
-            <p className={styles.text}>Maximize the impact of your campaigns with <strong>premium traffic</strong> that’s <strong>highly targeted</strong> and
-            guaranteed to increase your visibility and sales. Our proprietary network of traffic sources is
-            designed to deliver <strong>high-quality leads</strong> that convert at a higher rate.</p>
-          </div>
-        </div>
+        <Card
+          image="/assets/Premium-Traffic.png"
+          title="Premium Traffic Sources"
+          text={<>Maximize the impact of your campaigns with <strong>premium traffic</strong> that’s <strong>highly targeted</strong> and
+          guaranteed to increase your visibility and sales. Our proprietary network of traffic sources is
+          designed to deliver <strong>high-quality leads</strong> that convert at a higher rate.</>}
+        />
       </li>
     </ul>
   );
--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -1,12 +1,12 @@
 import Head from "next/head";
 import { LayoutBase } from '@/layouts/LayoutBase';
 import { Section1 } from './_sections/section1';
+import { Section2 } from './_sections/section2';
 import { Container } from '@/components/Container';
 import { Button } from '@/components/Button';
 import styles from './styles.module.css';
+
 // import Image from "next/image";
-
-
 import { TagTitle } from '@/components/TagTitle';
 
 export default function Home() {
@@ -19,48 +19,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <LayoutBase>
-        
         <Section1 />
-
-        <section>
-          <Container>
-            <div className={styles.content}>
-              <TagTitle style={{ color: "var(--color-orange)"}}>What we do?</TagTitle>
-              <h2 className={styles.title}>Engage, Convert, and Scale Your Business with Precision</h2>
-              <p className={styles.info}>We don’t just offer standard advertising solutions — we help you build real connections with your audience  <strong>social media ads, affiliate marketing,</strong> and <strong>email campaigns</strong> that are tailored to your business. Here’s how we stand out.</p>
-
-              <ul className={styles.cardGrid}>
-                <li className={styles.card}>
-                  {/* <img src="" alt="" /> */}
-                  <h3>CPC & CPL Performance Marketing</h3>
-                  <p>Our <strong>performance-based marketing</strong> models give you the <strong>power</strong> to only pay for what works. Whether it’s <strong>CPC</strong> (Cost Per Click) or <strong></strong> (Cost Per Lead), our model ensures that you’re investing in conversions and measurable results.</p>
-                </li>
-                <li className={styles.card}>
-                  {/* <img src="" alt="" /> */}
-                  <h3>Targeted Ads That Reach Your Ideal Audience</h3>
-                  <p>Our advanced targeting ensures that your message reaches the <strong>right people</strong> at the right time.  <strong>native ads</strong> and <strong>targeted social media campaigns,</strong> you’re not just throwing ads into the void — you’re speaking directly to a hand-picked audience primed to take action.</p>
-                </li>
-                <li className={styles.card}>
-                  {/* <img src="" alt="" /> */}
-                  <h3>Social Media & Email Marketing Expertise</h3>
-                  <p>Leverage the power of <strong>email marketing, social networking in marketing,</strong> and <strong>affiliate strategies</strong> to build a list of loyal subscribers, followers, and buyers. We craft email <strong>campaigns</strong> that cut through the noise, driving <strong>conversions</strong> and improving <strong>customer retention.</strong></p>
-                </li>
-                <li className={styles.card}>
-                  {/* <img src="" alt="" /> */}
-                  <h3>Affiliate Networks & Marketing</h3>
-                  <p>Scale your business with high-performing <strong>affiliate marketing</strong> strategies. By partnering with our extensive <strong>affiliate network</strong>, you gain access to a world of <strong>qualified leads</strong> and <strong>targeted marketing opportunities</strong> that will supercharge your growth.</p>
-                </li>
-                <li className={styles.card}>
-                  {/* <img src="" alt="" /> */}
-                  <h3>Premium Traffic Sources</h3>
-                  <p>Maximize the impact of your campaigns with <strong>premium traffic</strong> that’s <strong>highly targeted</strong> and
-                  guaranteed to increase your visibility and sales. Our proprietary network of traffic sources is
-                  designed to deliver <strong>high-quality leads</strong> that convert at a higher rate.</p>
-                </li>
-              </ul>
-            </div>
-          </Container>
-        </section>
+        <Section2 />
 
         <section style={{ backgroundColor: "var(--color-black)", color: "var(--color-white)" }}>
           <Container>
