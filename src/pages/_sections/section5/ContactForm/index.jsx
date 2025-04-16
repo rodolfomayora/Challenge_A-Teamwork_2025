2483b993077d116cb826adb5f8d450c60a6e9e18
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-16 15:50:23 -0400

    feat(pages): add contact forms

---
+++ src/pages/_sections/section5/ContactForm/index.jsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components/Button';
+import styles from './styles.module.css';
+
+import { Input } from './Input';
+import { Textarea } from './Textarea';
+
+export function ContactForm() {
+  return (
+    <form className={styles.ContactForm}>
+      <div className={styles.inputs}>
+        <div className={styles.textInputs}>
+          <Input
+            label="First Name"
+            placeholder="Enter Your First Name"
+          />
+          <Input
+            label="Last Name"
+            placeholder="Enter Your Last Name"
+          />
+        </div>
+        <Input
+          type="email"
+          label="Email"
+          placeholder="Email Address"
+        />
+        <Textarea
+          label="Your Message (optional)"
+          placeholder="Write Your Message"
+        />
+      </div>
+      <Button type="Button" style={{ height: 44 }}>Submit</Button>
+    </form>
+  );
+}
