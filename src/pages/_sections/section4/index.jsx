196f2daad01676c7f43e9b191365efd5d21a05fc
Rodolfo Mayora <rodolfomayora96@gmail.com>
2025-04-17 13:47:09 -0400

    feat(pages): carousel section-4 changes

--- src/pages/_sections/section4/index.jsx
+++ src/pages/_sections/section4/index.jsx
@@ -1,15 +1,14 @@
 import { Container } from '@/components/Container';
 import { TagTitle } from '@/components/TagTitle';
-import styles from './styles.module.css';
-
 import { Carousel } from './Carousel';
+import styles from './styles.module.css';
 
 export function Section4() {
   return (
     <section className={styles.Section4}>
       <Container>
         <div className={styles.content}>
-        <TagTitle style={{ color: "var(--color-orange)"}}>Our clients love us</TagTitle>
+        <TagTitle className={styles.customTagTitle}>Our clients love us</TagTitle>
           <h2 className={styles.title}>What Our Customers Have to Say</h2>
           <Carousel />
         </div>
